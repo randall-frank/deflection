937be8b561a0e5b45ca76f36aba5179c23d42b50
Randall Frank <frogboots.000@gmail.com>
2025-11-19 17:47:44 -0500

    Builds and runs
    Fix corruption in GAME.S
    Add missing binary tables in GFXTABLES
    Get build to work

--- build.py
+++ build.py
@@ -50,72 +50,17 @@ for name, address in files.items():
         sys.exit(1)
 os.chdir(orig_dir)
 
-'''
-        
-bins = {
-    "bin/LOADER.BIN": 0x2000, 
-    "bin/BIGFONT#061c00": 0x5C00,
-    "bin/SOUNDS#068900": 0x3900,     # reloc=0x8900
-    "bin/LOOKTBLS#060C00": 0x2E00,   # reloc=0x0E00
-    "bin/DECOMP_IMG.BIN": 0x4000,
-    "bin/diaflw_splash.fgr": 0x4100, 
-    "bin/diaflw_play.fgr": 0x4A00,   
-    "bin/HELP.BIN": 0x7000,          # this is overwritten after moved to text
-    "bin/GAME1.BIN": 0x6400,         # reloc=0x7400,
-    "bin/GAME2.BIN": 0x6C00,         # reloc=0x7C00,
-    "bin/IO2.BIN": 0x5000,           # reloc=0x9000,
-    "bin/OPENING2.BIN": 0x3000,      # reloc=0x8000,
-    "bin/PAC.BIN": 0x2800,           # reloc=0x0800,
-    "bin/WEAPONS.BIN": 0x6200,       # reloc=0x7200,
-    "bin/DRAWTAB.BIN": 0x4E00,       # reloc=0x4E00,
-}
-
-# 2000 - LOADER.S
-#     Reads from 2800-3000 and stores to 0800-1000
-#     Reads from 2100-2800 and stores to 9000-9800
-#     Reads from 3000-4000 and stores to 8000-9000
-#     Reads from 6000-7000 and stores to 7000-8000
-#     Paint the text page from 7000-7400
-#     JMP 8000
-
-log.info("Building DIAFLW.SYSTEM(SYS#ff) file...")
-
-# Build 'DIAFLW.SYSTEM,TSYS' from bins
-with open(os.path.join('src', 'DIAFLW_SYSTEM_orig.bin'), 'rb') as fp:
-    data = bytearray(fp.read())
-
-for name, addr in bins.items():
-    log.info(f"Loading {name} at {addr:04X}")
-    with open(name, "rb") as f:
-        local = bytearray(f.read())
-    offset = addr - 0x2000
-    length = len(local)
-    data[offset:offset+length] = local
-
-outname = "DIAFLW.SYSTEM#ff2000"
-with open(outname, "wb") as fp:
-    fp.write(data)
-log.info(f"Wrote system file: {outname}")
-
-log.info("Building DIAFLW.DOCS(TXT#04) file...")
-# Build docs file
-with open("diaflw_docs.txt", "r") as fp:
-    text = fp.read()
-    with open("SYSTEM/DIAFLW.DOCS#040000", "w") as out:
-        text = text.replace("V_NUM", version)
-        out.write(text)
-
 log.info("Building .2mg disk image...")
 # Create a release .2mg image
-rel_filename = "DIAFLW_Release.2mg"
+rel_filename = "Deflection_Release.2mg"
 try:
     os.remove(rel_filename)
 except Exception:
     pass
-cmd = [ciderpresscli, "create-disk-image", rel_filename, "800K", "prodos"]
+cmd = [ciderpresscli, "create-disk-image", rel_filename, "140K", "prodos"]
 result = subprocess.run(cmd, capture_output=True, text=True, check=True)
 log.info(f"Created release disk image: {result.stdout} {result.stderr}")
-cmd = [ciderpresscli, "rename", rel_filename, ":", f"DIAFLW_{version}"]
+cmd = [ciderpresscli, "rename", rel_filename, ":", f"DEFL_{version}"]
 result = subprocess.run(cmd, capture_output=True, text=True, check=True)
 log.info(f"Renamed release disk image: {result.stdout} {result.stderr}")
 
@@ -124,24 +69,9 @@ try:
     os.remove("SYSTEM/_FileInformation.txt")
 except Exception:
     pass
-cmd = [ciderpresscli, "add", "--strip-paths", "DIAFLW_Release.2mg", "SYSTEM"]
+cmd = [ciderpresscli, "add", "--strip-paths", rel_filename, "SYSTEM"]
 result = subprocess.run(cmd, capture_output=True, text=True, check=True)
 log.info(f"System files added to disk image: {result.stdout} {result.stderr}")
-# and DIAFLW.SYSTEM
-cmd = [ciderpresscli, "add", "--strip-paths", "DIAFLW_Release.2mg", outname]
-result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-log.info(f"System file added to disk image: {result.stdout} {result.stderr}")
-
-# Copy scenarios
-cmd = [ciderpresscli, "add", rel_filename, "SCENARIOS"]
-result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-log.info(f"Scenarios added to disk image: {result.stdout} {result.stderr}")
-# rename them to include trailing '.'
-for name in os.listdir("SCENARIOS"):
-    if os.path.isdir(os.path.join("SCENARIOS", name)):
-        cmd = [ciderpresscli, "rename", rel_filename, f"SCENARIOS/{name}", f"SCENARIOS/{name}."]
-        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
-        log.info(f"Renamed: SCENARIOS/{name}")
 
 for name in os.listdir("basic"):
     if name.upper().endswith(".ABAS"):
@@ -157,7 +87,11 @@ for name in os.listdir("basic"):
         os.remove(os.path.join("basic", root))
         log.info(f"Imported: basic/{name} as {root}")
 
-'''
+for name in os.listdir("bin"):
+    if not name.startswith("_"):
+        cmd = [ciderpresscli, "add", "--strip-paths", rel_filename, f"bin/{name}"]
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+        log.info(f"Imported: {name}")
 
 log.info(f"Build v{version} complete.")
  
